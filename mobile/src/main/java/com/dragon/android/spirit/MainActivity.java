f9c0545087eec51518ba4a582e5b88ae7ae9e797
lostoy <raymond007.lee@gmail.com>
2018-01-14 14:55:50 +0800

    no message

--- mobile/src/main/java/com/dragon/android/spirit/MainActivity.java
+++ mobile/src/main/java/com/dragon/android/spirit/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
+        TextView tv = (TextView) findViewById(R.id.text);
         tv.setText(stringFromJNI());
 
     }
