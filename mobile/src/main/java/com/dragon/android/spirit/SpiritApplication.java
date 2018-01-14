ad327822f5b48458ebb9dc5997b752c3891c66b8
lostoy <raymond007.lee@gmail.com>
2018-01-14 20:00:15 +0800

    add code

---
+++ mobile/src/main/java/com/dragon/android/spirit/SpiritApplication.java
@@ -0,0 +1,15 @@
+package com.dragon.android.spirit;
+
+import android.app.Application;
+
+/**
+ * Created by raymondlee on 2018/1/14.
+ */
+public class SpiritApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
---
+++ mobile/src/main/java/com/dragon/android/spirit/utilities/AppProtecter.java
@@ -0,0 +1,7 @@
+package com.dragon.android.spirit.utilities;
+
+/**
+ * Created by raymondlee on 2018/1/14.
+ */
+public class AppProtecter {
+}
